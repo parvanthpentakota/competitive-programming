0f07601174de22f8e2a0166073d581705fe4c8b2
Parvanth Pentakota <parvanthpentakota2316@gmail.com>
2026-08-03 22:17:50 +0530

    Adding solution for J-Multiples

---
+++ Codeforces/Practice/J-Multiples.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solution(){
+        int a, b;
+    cin>> a >> b;
+ 
+    if(a % b == 0 || b % a == 0){
+        cout<<"Multiples"<<endl;
+    }
+   
+    else{
+        cout<<"No Multiples"<<endl;
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    solution();
+    return 0;
+}
