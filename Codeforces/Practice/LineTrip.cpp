ede53b9fbc158ef5bd3d3c8a3ff88277f76d7da7
Parvanth Pentakota <parvanthpentakota2316@gmail.com>
2026-08-05 21:27:27 +0530

    Adding solution for LineTrip problem

---
+++ Codeforces/Practice/LineTrip.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solution() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int n, x;
+        cin >> n >> x;
+
+        vector<int> a(n);
+
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
+
+        int ans = a[0];
+
+        for (int i = 1; i < n; i++)
+            ans = max(ans, a[i] - a[i - 1]);
+
+        ans = max(ans, 2 * (x - a[n - 1]));
+
+        cout << ans << '\n';
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    solution();
+    return 0;
+}
