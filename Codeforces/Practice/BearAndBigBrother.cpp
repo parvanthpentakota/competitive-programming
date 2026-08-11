a0eda28445e8fbb4a192fda4a7198ba756d0d3df
Parvanth Pentakota <parvanthpentakota2316@gmail.com>
2026-08-11 21:52:34 +0530

    Adding solution for Bear And Big Brother problem

---
+++ Codeforces/Practice/BearAndBigBrother.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solution(){
+     int a, b;
+    cin >> a >> b;
+
+    int years = 0;
+
+    while (a <= b) {
+        a *= 3;
+        b *= 2;
+        years++;
+    }
+
+    cout << years;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    solution();
+    return 0;
+}
