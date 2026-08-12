df608517cebc0571ddb6607584aa654ce62d70fa
Parvanth Pentakota <parvanthpentakota2316@gmail.com>
2026-08-12 23:15:43 +0530

    Adding solution for elephant problem

---
+++ Codeforces/Practice/Elephant.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solution() {
+    int x;
+    cin >> x;
+
+    cout << (x + 4) / 5 << '\n';
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    solution();
+    return 0;
+}
