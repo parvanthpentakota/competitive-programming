36b1988ccd44d453b19a82a1cb7c92f2ea624ee8
Parvanth Pentakota <parvanthpentakota2316@gmail.com>
2026-09-24 23:13:03 +0530

    solved Candies and Two Sisters problem

---
+++ Codeforces/Practice/1335A.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solution(){
+    int n;
+    cin >> n;
+
+    if(n <= 2){
+        cout << 0 << '\n';
+    }
+    else if(n % 2 == 0){
+        cout << n / 2 - 1 << '\n';
+    }
+    else{
+        cout << n / 2 << '\n';
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    solution();
+    return 0;
+}
