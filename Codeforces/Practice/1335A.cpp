f9bc00da81827011b43ee73ffdbfbbc1499b5517
Parvanth Pentakota <parvanthpentakota2316@gmail.com>
2026-09-24 23:25:39 +0530

    modified Candies and Two Sisters

--- Codeforces/Practice/1335A.cpp
+++ Codeforces/Practice/1335A.cpp
@@ -2,17 +2,14 @@
 using namespace std;
 
 void solution(){
-    int n;
-    cin >> n;
+    int t;
+    cin >> t;
 
-    if(n <= 2){
-        cout << 0 << '\n';
-    }
-    else if(n % 2 == 0){
-        cout << n / 2 - 1 << '\n';
-    }
-    else{
-        cout << n / 2 << '\n';
+    while(t--){
+        long long n;
+        cin >> n;
+
+        cout << (n - 1) / 2 << '\n';
     }
 }
 
